16f4afb69120967db99a0abcc13df514d48c485a
hase <hm.works2@gmail.com>
2020-07-12 21:27:30 +0900

    sign in function

--- whisperer/src/components/Header.vue
+++ whisperer/src/components/Header.vue
@@ -5,12 +5,44 @@
       <h1>whisp.</h1>
     </router-link>
     <div class="btns">
-      <button>
+      <button @click="signIn">
         <fa icon="user" />
       </button>
     </div>
   </header>
 </template>
+<script>
+  // Firebaseのプラグインをインポート
+  import firebase from 'firebase'
+  // mainからauthをインポートする
+  import { auth } from '../main'
+  export default {
+    methods: {
+      signIn () {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider)
+        .then((result) => {
+          // ユーザ名をアラートで表示
+          alert('Hello, '+result.user.displayName+'!')
+        })
+      },
+      signOut () {
+        if (window.confirm('Are You Sure to Sign Out?')) {
+          auth.signOut()
+          .then(() => {
+            // サインアウト後、アラートを出す
+            alert('You Safely Signed Out.')
+            // トップページに戻る
+            this.$router.push('/'),
+            // ページをリロード
+            location.reload()
+          })
+        }
+      }
+    }
+  }
+</script>
+signInの中身
 <style lang="stylus" scoped>
 header
   position fixed
