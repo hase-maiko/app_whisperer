077d232350623df4b183c9b8e54b7744a0959e0f
hase <hm.works2@gmail.com>
2020-07-12 16:51:51 +0900

    firebase setting

--- whisperer/src/main.js
+++ whisperer/src/main.js
@@ -18,6 +18,30 @@ library.add(
   faSignOutAlt,
   faEllipsisV
 )
+
+// firebaseの初期化
+import { firestorePlugin } from 'vuefire'
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+Vue.use(firestorePlugin)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAaTuT1g4arXFmtLSynFaAyXW0pJZa-Oq8",
+  authDomain: "whisperer-b2260.firebaseapp.com",
+  databaseURL: "https://whisperer-b2260.firebaseio.com",
+  projectId: "whisperer-b2260",
+  storageBucket: "whisperer-b2260.appspot.com",
+  messagingSenderId: "538624485413",
+  appId: "1:538624485413:web:850729769be09f2c7177cc",
+  measurementId: "G-BRJS7ZFQE4"
+})
+
+// dbという名前でアプリ内でFirestoreデータベースを使えるようにする
+export const db = firebase.firestore()
+// authという名前でユーザー管理のためのサービスを使えるようにする
+export const auth = firebase.auth()
+
 // FontAwesomeIconをfaという略式で呼べるように設定
 Vue.component('fa', FontAwesomeIcon)
 Vue.config.productionTip = false
